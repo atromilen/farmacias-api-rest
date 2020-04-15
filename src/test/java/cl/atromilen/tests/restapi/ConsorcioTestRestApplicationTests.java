7cdaba1f63547c89b939de6044f0bcdc82839a03
Alvaro Tromilen <alvaro.tromilen@gmail.com>
2020-04-14 23:58:24 -0400

    Initial commit

---
+++ src/test/java/cl/atromilen/tests/restapi/ConsorcioTestRestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.atromilen.tests.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsorcioTestRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
